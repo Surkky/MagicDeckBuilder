d82f8193c27baad87b1138258d7d41da69c9f65e
MariFructuoso <marifructuosog718@gmail.com>
2025-05-05 19:44:50 +0200

    Implement login interface and rename files
    Added the login interface and renamed classes and FXML files for consistency

--- MagicDeckBuilder/src/main/java/org/example/magicdeckbuilder/MainApplication.java
+++ MagicDeckBuilder/src/main/java/org/example/magicdeckbuilder/MainApplication.java
@@ -5,14 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Login - Magic Deck Builder");
         stage.setScene(scene);
         stage.show();
     }
