4709f1c7ee5b2bd6655ff25fd31782c45808fad6
MariFructuoso <marifructuosog718@gmail.com>
2025-05-19 21:21:19 +0200

    Added Back button in CreateDeck

--- MagicDeckBuilder/MagicDeckBuilder/src/main/java/org/example/magicdeckbuilder/MainApplication.java
+++ MagicDeckBuilder/MagicDeckBuilder/src/main/java/org/example/magicdeckbuilder/MainApplication.java
@@ -1,5 +1,4 @@
 package org.example.magicdeckbuilder;
-import javafx.scene.paint.Color;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
--- MagicDeckBuilder/MagicDeckBuilder/src/main/java/org/example/magicdeckbuilder/controller/CreateDeckController.java
+++ MagicDeckBuilder/MagicDeckBuilder/src/main/java/org/example/magicdeckbuilder/controller/CreateDeckController.java
@@ -8,6 +8,11 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import java.io.IOException;
 
 
 public class CreateDeckController implements Initializable {
@@ -16,8 +21,6 @@ public class CreateDeckController implements Initializable {
     @FXML private StackPane rootPane;
     @FXML private VBox cardContainer;
 
-
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Image background = new Image(getClass().getResource("/images/createDeck_background.jpg").toExternalForm());
@@ -25,5 +28,20 @@ public class CreateDeckController implements Initializable {
 
         backgroundImage.fitWidthProperty().bind(rootPane.widthProperty());
         backgroundImage.fitHeightProperty().bind(rootPane.heightProperty());
+    }
+    @FXML
+    private void goBack() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/magicdeckbuilder/main.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 1152, 768);
+            scene.getStylesheets().add(getClass().getResource("/styles/main.css").toExternalForm());
+
+            Stage stage = (Stage) rootPane.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
